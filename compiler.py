d961788a920e010f4915712a4475b44f4638536d
pernydev <83672513+pernydev@users.noreply.github.com>
2023-01-12 01:13:23 +0200

    Added simple minification

--- compiler.py
+++ compiler.py
@@ -1,6 +1,6 @@
 import jinja2
 import os
-
+import re
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 compiled_dir = os.path.join(os.path.dirname(__file__), 'compiled')
@@ -13,6 +13,10 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
 def compile_templates():
     for root, dirs, files in os.walk(template_dir):
         for filename in files:
+            with open(os.path.join(root, filename), 'r') as f:
+                content = f.read()
+            if content.startswith('<!-- skip -->'):
+                continue
             if filename.endswith('.html'):
                 path = os.path.relpath(os.path.join(root, filename), template_dir)
                 output_path = os.path.join(compiled_dir, path)
@@ -21,8 +25,14 @@ def compile_templates():
                     os.makedirs(output_dir)
                 print("Compiling %s" % path)
                 template = env.get_template(path)
+                rendered = template.render()
+                rendered = " ".join(rendered.split())
+                rendered = rendered.replace("> <", "><")
+                commentpattern = re.compile(r'<!--.*?-->', re.DOTALL)
+                rendered = commentpattern.sub('', rendered)
+                
                 with open(output_path, 'w') as f:
-                    f.write(template.render())
+                    f.write(rendered)
 
 if __name__ == '__main__':
     compile_templates()
